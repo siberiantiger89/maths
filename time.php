0bc2454592d9166cbd22f834e79e55093337ae4b
srg89 <p08277108@myemail.dmu.ac.uk>
2019-04-30 22:42:47 +0100

    added a 12-hour dropdown menu to form

--- time.php
+++ time.php
@@ -6,24 +6,36 @@
 			$a=$_GET['a'];
 			$b=$_GET['b'];
 			$c=$_GET['c'];
+			$d=$_GET['d'];
 
 			//filter user input
 			$a=filter_var($a,FILTER_SANITIZE_STRING);
-			$b=filter_var($b,FILTER_SANITIZE_NUMBER_INT);
+			$b=filter_var($b,FILTER_SANITIZE_STRING);
 			$c=filter_var($c,FILTER_SANITIZE_NUMBER_INT);
+			$d=filter_var($d,FILTER_SANITIZE_NUMBER_INT);
 
 			//substr user input
 			$a=substr($a,0,5);
-			$b=substr($b,0,200);
+			$b=substr($b,0,2);
 			$c=substr($c,0,200);
+			$d=substr($d,0,200);
 
 			//validate user input
-			if((preg_match("/^(?:1[012]|0[0-9]):[0-5][0-9]$/",$a))&&(is_numeric($b))&&(is_numeric($c))){
+			if((preg_match("/^(?:1[012]|0[0-9]):[0-5][0-9]$/",$a))&&(($b==='am')||($b==='pm'))&&(is_numeric($c))&&(is_numeric($d))){
 				$time_array=explode(':',$a);
-				if(($time_array[1]+($c%60))>=60){
+				if(($time_array[1]+($d%60))>=60){
 					$time_array[0]++;
 				}
-				$answer=((str_pad((($time_array[0]+$b+(floor($c/60)))%12),2,0,STR_PAD_LEFT)).':'.(str_pad(((($time_array[1]+($c%60)))%60),2,0,STR_PAD_LEFT)));
+				if((floor(($time_array[0]+$c+floor(($time_array[1]+$d)/60))/12))&1){
+					if($b==='am'){
+						$period='pm';
+					} else {
+						$period='am';
+					}
+				} else {
+					$period=$b;
+				}
+				$answer=((str_pad((($time_array[0]+$c+(floor($d/60)))%12),2,0,STR_PAD_LEFT)).':'.(str_pad(((($time_array[1]+($d%60)))%60),2,0,STR_PAD_LEFT))).' '.$period;
 			} else {
 				$answer='Invalid input. Please try again.';
 			}
@@ -31,6 +43,7 @@
 			$a='';
 			$b='';
 			$c='';
+			$d='';
 			$answer='';
 		}
 	}
@@ -44,7 +57,7 @@
 <h1>Mathematical Functions in PHP</h1>
 <h2>Simple Time Calculating in PHP</h2>
 <form action="time.php" method="get">
-	<strong>Time: </strong><input type="text" name="a" value="<?php echo $a; ?>" /> + <input type="text" name="b" value="<?php echo $b; ?>" /> hours <input type="text" name="c" value="<?php echo $c; ?>" /> minutes = <?php echo $answer; ?><br/><br/>
+	<strong>Time: </strong><input type="text" name="a" value="<?php echo $a; ?>" /> <select name="b"><option value="am" <?php if($b==='am'){echo 'selected';}?>>am</option><option value="pm" <?php if($b==='pm'){echo 'selected';}?>>pm</option></select> + <input type="text" name="c" value="<?php echo $c; ?>" /> hours <input type="text" name="d" value="<?php echo $d; ?>" /> minutes = <?php echo $answer; ?><br/><br/>
 	<input type="submit" value="Calculate Time" /><br/><br/>
 </form>
 <a href="index.html" />Index Page</a>
